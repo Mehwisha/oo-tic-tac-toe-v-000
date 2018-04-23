534cb140385420c48804cd2d162dd45ae91cda9c
Mehwish Aziz <mehwisha@msn.com>
2018-04-23 23:16:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,6 +19,45 @@ def display_board
    puts "-----------"
    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
  end
+def input_to_index
 
+end
+def move
+
+end
+def position_taken?
+
+end
+def valid_move?
+
+end
+def turn_count
+@board.count{|token| token == "X" || token == "O"}
+end
+def current_player
+ turn_count % 2 == 0 ? "X" : "O"
+end
+def turn
+
+end
+def won?
+
+end
+def full?
+
+end
+def draw?
+
+
+end
+def over?
+
+end
+def winner
+
+end
+def play
+
+end
 
 end
