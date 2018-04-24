ad04a8b430a059108a3ce2c0e2923bff872753e4
Mehwish Aziz <mehwisha@msn.com>
2018-04-24 01:20:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,9 +69,18 @@ def over?
 won?|| draw?
 end
 def winner
-
+  if winning_combo = won?
+      @board[winning_combo.first]
+    end
 end
 def play
-
+  while !over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
 end
 end
