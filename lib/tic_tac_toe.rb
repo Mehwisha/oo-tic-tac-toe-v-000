fb3d137f6e2ccb81deb4eb7ee94576ae749da89b
Mehwish Aziz <mehwisha@msn.com>
2018-04-24 01:15:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,9 +54,8 @@ end
 
 def won?
   WIN_COMBINATIONS.detect do |combo|
-    @board[combo[0]] == @board[combo[1]] &&  @board[combo[1]] == @board[combo[2]] && position_taken?(@board, combo[0])
+    @board[combo[0]] == @board[combo[1]] &&  @board[combo[1]] == @board[combo[2]] && position_taken?(combo[0])
   end
-  return combo
 end
 
 def full?
